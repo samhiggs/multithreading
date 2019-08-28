ec9ef4df137b861100b366ce836b374f16b58aa7
samhiggs <shig2991@uni.sydney.edu.au>
2019-08-28 12:37:38 +1000

    added BL to template

--- a1/traffic_lights_template.c
+++ a1/traffic_lights_template.c
@@ -22,128 +22,182 @@ typedef struct vehicle_object
     char direction[4];
 } vehicle_t;
 
+enum Direction { n2s, s2n, e2w, w2e, n2w, s2e, } dctn;
 
 void * mini_controller_routine(void *);
 void * vehicle_routine(void *);
 
-//declare global mutex and condition variables
+pthread_mutex_t intersection_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-... // you need to add something here
-
-
-
+pthread_cond_t ns_mini_cntrl, ew_mini_cntrl, turn_mini_cntrl;
+vehicle_t *vehicle;
 int main(int argc, char ** argv)
 {
     int n_vehicles; //total number of vehicles
 	int vehicle_rate; //vehicle arrival rate to the intersection
 	int min_interval; // min time interval between two consecutive vehicles to pass the intersection
-	
-	//more variable declarations
-	
-	... // you need to add something here
-
-	
-	
-    // Initialize mutex and condition variables
- 
-	... // you need to add something here if necessary
-	
-
-	
-	//allocate memory for mini_controllers 
-	mini_controller_thrd_id = malloc(3 * sizeof(pthread_t)); //system thread ids
-    if (mini_controller_thrd_id == NULL)
+    int rc, i, drct;
+    int g_t;
+    int ns_c, sn_c, ew_c, we_c, nw_c, se_c;
+    int n_cntrls = 3; //number of mini controllers.
+    pthread_t *vehicle_thread_ids, *mini_cntrl_thread_ids;
+    mini_cntrl_t *mini_controller;
+    rc = pthread_cond_init(&ns_mini_cntrl, NULL);
+    if(rc)
     {
-        fprintf(stderr, "mini_controlle_thrds_id out of memory\n");
+        fprintf(stderr, "ERROR; return code from pthread_cond_init(ns) is %d\n", rc);
         exit(1);
+	}
+    rc = pthread_cond_init(&ew_mini_cntrl, NULL);
+    if(rc)
+    {
+        fprintf(stderr, "ERROR; return code from pthread_cond_init(ew) is %d\n", rc);
+        exit(1);
+	}
+    rc = pthread_cond_init(&turn_mini_cntrl, NULL);
+    if(rc)
+    {
+        fprintf(stderr, "ERROR; return code from pthread_cond_init(turn)  is %d\n", rc);
+        exit(1);
+	}
+	//allocate memory for mini_controllers 
+	mini_cntrl_thread_ids = calloc(3, sizeof(pthread_t)); //system thread ids
+    if (mini_cntrl_thread_ids == NULL)
+    { 
+        fprintf(stderr, "mini_cntrl_thread_ids out of memory\n");
+        exit(2);
     }
-    mini_controller = malloc(3 * sizeof(mini_cntrl_t)); //mini_controller objects
+    mini_controller = calloc(3, sizeof(mini_cntrl_t)); //mini_controller objects
+    if (mini_controller == NULL)
+    { 
+        fprintf(stderr, "mini controller out of memory\n");
+        exit(2);
+    }
+
+    //*****MAKE SURE I REMOVE THIS BEFORE SUBMITTING!*******//
+    if(argc != 5)
+    {
+        // ask for the total number of vehicles.
+        printf("Enter the total number of vehicles (int): ");
+        scanf("%d", &n_vehicles);
+        //ask for vehicles' arrival rate to the intersection
+        printf("Enter vehicles arrival rate (int): \n");
+        scanf("%d", &vehicle_rate);
+        //ask for the minimum time interval t (in seconds) between any two 
+        //consecutive vehicles in one direction to pass through the intersection
+        printf("Enter minimum interval between two consecutive vehicles (int): \n");
+        scanf("%d", &min_interval);
+        //ask for green time for each mini_controller
+        printf("Enter green time for forward-moving vehicles on trunk road (int): ");
+        scanf("%d", &g_t);
+        mini_controller[0].time_green = g_t;
+        printf("Enter green time for vehicles on minor road (int): ");
+        scanf("%d", &g_t);
+        mini_controller[1].time_green = g_t;	
+        printf("Enter green time for right-turning vehicles on trunk road (int): ");
+        scanf("%d", &g_t);
+        mini_controller[2].time_green = g_t;
+    } else 
+    {
+        n_vehicles = atoi(argv[1]);
+        vehicle_rate = atoi(argv[2]);
+        min_interval = atoi(argv[3]);
+        int green_time = atoi(argv[4]);
+        mini_controller[0].time_green = green_time;
+        mini_controller[1].time_green = green_time;
+        mini_controller[2].time_green = green_time;
+    }
+	
+	//create mini_controller threads 
     if (mini_controller == NULL)
     {
         fprintf(stderr, "mini_controller out of memory\n");
-        exit(1);
+        exit(2);
+    }
+    for(i = 0; i < n_cntrls; ++i)
+    {
+        mini_controller[i].id = i;
+        mini_controller[i].min_interval = min_interval;
+        printf("Creating mini controller %d \n", i);
+        rc = pthread_create(&mini_cntrl_thread_ids[i], NULL, mini_controller_routine, (void*)&mini_controller[i]);
+        if (rc) 
+        {
+            fprintf(stderr, "return code from pthread_create(ns controller) is %d\n", rc);
+            exit(3);
+        }
     }
 
-	// input parameters
-    // ask for the total number of vehicles.
-    printf("Enter the total number of vehicles (int): ");
-    scanf("%d", &n_vehicles);
-
-	//ask for vehicles' arrival rate to the intersection
-	printf("Enter vehicles arrival rate (int): \n");
-	scanf("%d", &vehicle_rate);
-	
-	//ask for the minimum time interval t (in seconds) between any two 
-	//consecutive vehicles in one direction to pass through the intersection
-	printf("Enter minimum interval between two consecutive vehicles (int): \n");
-	scanf("%d", &min_interval);
-	
-    //ask for green time for each mini_controller
-    printf("Enter green time for forward-moving vehicles on trunk road (int): ");
-    scanf("%d", &g_t);
-    mini_controller[0].time_green = g_t;
-    printf("Enter green time for vehicles on minor road (int): ");
-    scanf("%d", &g_t);
-    mini_controller[1].time_green = g_t;	
-    printf("Enter green time for right-turning vehicles on trunk road (int): ");
-    scanf("%d", &g_t);
-    mini_controller[2].time_green = g_t;
-	
-
-	//allocate memory for vehicles 
-	
-	... // you need to add something here
-	
-
-	
-    //create mini_controller threads 
-	
-	... // you need to add something here
-	
-	
-	
+    //allocate memory for vehicles 
+    vehicle = calloc(n_vehicles, sizeof(vehicle_t));
 	//create vehicles threads
-
-	... // you need to add something here
-	
-	
-    
+    ns_c = sn_c = ew_c = we_c = nw_c = se_c = 0;
+    srand(time(0));
+    for(i = 0; i < n_vehicles; ++i) 
+    {
+		sleep((int)rand() % vehicle_rate); 
+		drct = (int)rand()% 6;
+        switch(drct)
+        {
+            case n2s: 
+                strncpy(vehicle[i].direction, "n2s", 4);
+                vehicle[i].id = ns_c;
+                ++ns_c;
+            case s2n: 
+                strncpy(vehicle[i].direction, "s2n", 4);
+                vehicle[i].id = sn_c;
+                ++sn_c;
+            case e2w: 
+                strncpy(vehicle[i].direction, "e2w", 4);
+                vehicle[i].id = ew_c;
+                ++ew_c;
+            case w2e: 
+                strncpy(vehicle[i].direction, "w2e", 4);
+                vehicle[i].id = we_c;
+                ++we_c;
+            case n2w: 
+                strncpy(vehicle[i].direction, "n2w", 4);
+                vehicle[i].id = nw_c;
+                ++nw_c;
+            case s2e: 
+                strncpy(vehicle[i].direction, "s2e", 4);
+                vehicle[i].id = se_c;
+                ++se_c;
+        }
+        printf("Created a vehicle of direction %s\n...creating thread\n", vehicle[i].direction);
+		rc = pthread_create(&vehicle_thread_ids[i], NULL, vehicle_routine, (void *)&vehicle[i]);
+		if (rc) {
+			printf("ERROR; return code from pthread_create(vehicle) is %d\n", rc);
+			exit(4);
+		}
+    }	
 	//join and terminating threads.
-
-	... // you need to add something here
-	
-	
-
-    //deallocate allocated memory
-	
-	... // you need to add something here
-	
-
-	
+    for(i = 0; i < n_vehicles; ++i){
+        pthread_join(vehicle_thread_ids[i], NULL);
+    }
+    for(i = 0;i < n_cntrls; ++i){
+        pthread_cancel(mini_cntrl_thread_ids[i]);
+    }
+		
     //destroy mutex and condition variable objects
  
-	... // you need to add something here
-	
-	
-	
+    pthread_mutex_destroy(&intersection_mutex);
+    pthread_cond_destroy(&ns_mini_cntrl);
+    pthread_cond_destroy(&ew_mini_cntrl);
+    pthread_cond_destroy(&turn_mini_cntrl);
+
+    free(vehicle);
+    free(mini_cntrl_thread_ids);
+    free(vehicle_thread_ids);
+    free(mini_controller);
+
     exit(0);
 }
 
-void * mini_controller_routine(void * arg)
-{
-
-
-... // you need to implement mini_controller routine
-
-
+void * mini_controller_routine(void * arg) {
+    printf("Mini controller routine exists!\n");
 }
 
-void * vehicle_routine(void * arg)
-{
-
-
-... // you need to implement vehicle routine
-
-
+void * vehicle_routine(void * arg){
+    printf("Vehicle routine exists!\n");
 }
 
